e7f11821c4498cd64363a1de1d8d36fb98ba804d
Patrick Beggan <pbeggan@loannex.com>
2022-03-11 14:24:08 -0600

    Rename contract to ERC721Minter. Add dev mint counter.

--- contracts/ERC721Minter.sol
+++ contracts/ERC721Minter.sol
@@ -7,10 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract Minter is 
-  ERC721, 
-  Ownable 
-{
+contract ERC721Minter is ERC721, Ownable {
   using Counters for Counters.Counter;
   
   enum Status {
@@ -23,6 +20,7 @@ contract Minter is
   uint256 private constant AMOUNT_FOR_DEVS = 5;
 
   Counters.Counter private _tokenIdTracker;
+  Counters.Counter private _devMintCounter;
 
   string private _baseTokenURI;
 
@@ -40,8 +38,6 @@ contract Minter is
     string memory baseTokenURI
     ) ERC721(name, symbol) {
     _baseTokenURI = baseTokenURI;
-
-    // We want to start at 1
     _tokenIdTracker.increment();
   }
 
@@ -55,28 +51,49 @@ contract Minter is
     _baseTokenURI = baseTokenURI;
   }
 
-  // STEPS FOR MINT
+  /**
+  @notice Mint tokens reserved for devs. The intention is to call this once — passing in `AMOUNT_FOR_DEVS`.
+  However, there are cases where we may need to call this again (e.g., reach allow list deadline to mint with tokens still available).
+  @param count The number of tokens to mint
+  */
+  function devMint(uint256 count) external onlyOwner {
+		for (uint256 i = 0; i < count; i++) {
+			_mintTo(_msgSender());
+      _devMintCounter.increment();
+		}
+	}
 
   /**
-  * 1. Set merkle root before minting.
+  @notice Get the number of tokens minted by devs.
+  */
+  function getDevMintCount() external view returns(uint256) {
+    return _devMintCounter.current();
+  }
+
+  /**
+  @notice Set merkle root. The merkle tree is generated off-chain from a list of addresses.
+  The intention is to call this once.
   */
   function setMerkleRoot(bytes32 root) external onlyOwner {
+    require(!metadataIsFrozen, "Metadata is permanently frozen");
+
     merkleRoot = root; 
   }  
 
   /**
-  * 2. Make sale active (or pause sale if necessary).
+  @notice Make sale active (or pause sale if necessary).
   */
   function flipSaleState() external onlyOwner {
     saleIsActive = !saleIsActive;
   }
 
   /**
-  * 3a. Allowlist mint.
+  @notice Mints a token to the caller if requirements are met.
+  @param proof TODO
   */
-  function allowlistMint(bytes32[] memory _proof) external payable {
+  function allowlistMint(bytes32[] memory proof) external payable {
     require(saleIsActive, "Sale must be active to claim");
-    require(checkStatus(_msgSender(), _proof) == Status.Eligible, "Account must be eligible to claim");
+    require(checkStatus(_msgSender(), proof) == Status.Eligible, "Account must be eligible to claim");
     require(msg.value >= mintPrice, "Insufficient funds provided");
 
     _mintTo(_msgSender());
@@ -89,24 +106,15 @@ contract Minter is
   }
 
   /**
-  * 3b. Dev mint.
+  @notice Withdraw balance to the supplied address.
   */
-  function devMint(uint256 count) external onlyOwner {
-		for (uint256 i = 0; i < count; i++) {
-			_mintTo(_msgSender());
-		}
-	}
-
-  /**
-  * 4. Withdraw funds to supplied address.
-  */
-  function withdrawTo(address address_) external onlyOwner {
-		(bool success, ) = address_.call{value: address(this).balance}("");
+  function withdrawTo(address to) external onlyOwner {
+		(bool success, ) = to.call{value: address(this).balance}("");
 		require(success, "Transfer failed");
 	}
 
   /**
-  * 5. Freeze metadata when ready to permanently lock.
+  @notice Freeze metadata when ready to permanently lock.
   */
   function freezeMetadata() external onlyOwner {
     require(!metadataIsFrozen, "Metadata is already frozen");
@@ -114,10 +122,8 @@ contract Minter is
     metadataIsFrozen = true;
   }
 
-  // PUBLIC METHODS
-
   /**
-  * Check allowlist eligibility for supplied address.
+  @notice Check allow list eligibility for the supplied address.
   */
   function checkStatus(address _account, bytes32[] memory _proof) public view returns (Status) {
     require(merkleRoot != "", "Merkle root must not be empty");
@@ -130,24 +136,23 @@ contract Minter is
   }
 
   /**
-  * Update mint price if necessary.
+  @notice Update mint price if necessary.
   */
   function setMintPrice(uint256 newPrice) external onlyOwner {
     mintPrice = newPrice;
   }
 
   /**
-  * Get current token id.
+  @notice Get next available token id. 
+  Subtract 1 from the return value to get the number of tokens already minted.
   */
-  function getCurrentTokenId() external view returns (uint256) {
+  function getNextTokenId() external view returns (uint256) {
     return _tokenIdTracker.current();
   }
 
-  // PRIVATE METHODS
-
   /**
-  * Verify that the given leaf belongs to a given tree 
-  * using its root for comparison.
+  @notice Verify that the given leaf belongs to a given tree 
+  using its root for comparison.
   */
 	function _verifyMerkleLeaf(  
     bytes32 _leafNode,  
@@ -160,14 +165,14 @@ contract Minter is
   }
 
   /**
-  * Create merkle leaf from supplied address.
+  @notice Create a merkle leaf from the supplied address.
   */
   function _generateMerkleLeaf(address _account) internal pure returns (bytes32) {  
     return keccak256(abi.encodePacked(_account)); 
   }
 
   /**
-  * Mint to supplied address.
+  @notice Mint to the supplied address.
   */
   function _mintTo(address to) private {
     require(_tokenIdTracker.current() <= AMOUNT_FOR_ALLOWLIST + AMOUNT_FOR_DEVS, "Reached max token supply");
