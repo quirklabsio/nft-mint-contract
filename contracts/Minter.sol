4d676bde727b812c0f4479353250060b83bf443d
Patrick Beggan <pbeggan@loannex.com>
2022-03-10 19:29:57 -0600

    Add descriptive comments

--- contracts/Minter.sol
+++ contracts/Minter.sol
@@ -7,78 +7,121 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract Minter is ERC721, Ownable {
+contract Minter is 
+  ERC721, 
+  Ownable 
+{
   using Counters for Counters.Counter;
   
-	enum Status {
-		Eligible,
-		AlreadyClaimed,
-		NotEligible
-	}
+  enum Status {
+    Eligible,
+    AlreadyClaimed,
+    NotEligible
+  }
 
   uint256 private constant AMOUNT_FOR_ALLOWLIST = 995;
   uint256 private constant AMOUNT_FOR_DEVS = 5;
 
-  Counters.Counter private _tokenIds;
-  string private _tokenBaseURI;
+  Counters.Counter private _tokenIdTracker;
+
+  string private _baseTokenURI;
 
   bytes32 public merkleRoot; 
+
+  bool public saleIsActive = false;
 	bool public metadataIsFrozen = false;
   uint256 public mintPrice = 0.1 ether;
+
   mapping(address => bool) public allowlistClaimed;
 
   constructor(
-    string memory name, 
-    string memory symbol, 
-    string memory uri
+    string memory name,
+    string memory symbol,
+    string memory baseTokenURI
     ) ERC721(name, symbol) {
-    _tokenBaseURI = uri;
-    _tokenIds.increment(); // Start at 1
+    _baseTokenURI = baseTokenURI;
+
+    // We want to start at 1
+    _tokenIdTracker.increment();
+  }
+
+  function _baseURI() internal view virtual override returns (string memory) {
+    return _baseTokenURI;
+  }
+
+  function setBaseTokenURI(string memory baseTokenURI) external onlyOwner {
+    require(!metadataIsFrozen, "Metadata is permanently frozen");
+
+    _baseTokenURI = baseTokenURI;
   }
 
   // STEPS FOR MINT
 
-  // 1. Set merkle root
+  /**
+  * 1. Set merkle root before minting.
+  */
   function setMerkleRoot(bytes32 root) external onlyOwner {
     merkleRoot = root; 
   }  
 
-  // 2a. Allowlist mint
+  /**
+  * 2. Make sale active (or pause sale if necessary).
+  */
+  function flipSaleState() external onlyOwner {
+    saleIsActive = !saleIsActive;
+  }
+
+  /**
+  * 3a. Allowlist mint.
+  */
   function allowlistMint(bytes32[] memory _proof) external payable {
-    require(_tokenIds.current() <= AMOUNT_FOR_ALLOWLIST, "Reached max allowlist supply");
-    require(checkStatus(msg.sender, _proof) == Status.Eligible, "Account is not eligible to claim");
+    require(saleIsActive, "Sale must be active to claim");
+    require(checkStatus(_msgSender(), _proof) == Status.Eligible, "Account must be eligible to claim");
     require(msg.value >= mintPrice, "Insufficient funds provided");
 
-    _mintTo(msg.sender);
-    allowlistClaimed[msg.sender] = true;
+    _mintTo(_msgSender());
+    allowlistClaimed[_msgSender()] = true;
 
     // Refund if over
     if (msg.value > mintPrice) {
-      payable(msg.sender).transfer(msg.value - mintPrice);
+      payable(_msgSender()).transfer(msg.value - mintPrice);
     }
   }
 
-  // 2b. Dev mint
+  /**
+  * 3b. Dev mint.
+  */
   function devMint(uint256 count) external onlyOwner {
 		for (uint256 i = 0; i < count; i++) {
-			_mintTo(msg.sender);
+			_mintTo(_msgSender());
 		}
 	}
 
-  // 3. Withdraw funds
-  function withdrawFunds(address address_) external onlyOwner {
+  /**
+  * 4. Withdraw funds to supplied address.
+  */
+  function withdrawTo(address address_) external onlyOwner {
 		(bool success, ) = address_.call{value: address(this).balance}("");
 		require(success, "Transfer failed");
 	}
 
-  // 4. Freeze metadata
+  /**
+  * 5. Freeze metadata when ready to permanently lock.
+  */
   function freezeMetadata() external onlyOwner {
     require(!metadataIsFrozen, "Metadata is already frozen");
+
     metadataIsFrozen = true;
   }
 
-  // Check account status
+  // PUBLIC METHODS
+
+  /**
+  * Check allowlist eligibility for supplied address.
+  */
   function checkStatus(address _account, bytes32[] memory _proof) public view returns (Status) {
+    require(merkleRoot != "", "Merkle root must not be empty");
+
     return !_verifyMerkleLeaf(_generateMerkleLeaf(_account), merkleRoot, _proof) 
             ? Status.NotEligible
             : allowlistClaimed[_account]
@@ -86,23 +129,26 @@ contract Minter is ERC721, Ownable {
               : Status.Eligible;
   }
 
-  // Update token base URI
-  function setBaseURI(string memory uri) external onlyOwner {
-    require(!metadataIsFrozen, "Metadata is permanently frozen");
-    _tokenBaseURI = uri;
-  }
-
-  // Update mint price
+  /**
+  * Update mint price if necessary.
+  */
   function setMintPrice(uint256 newPrice) external onlyOwner {
     mintPrice = newPrice;
   }
 
-  // Get next tokenId
-  function getNextTokenId() external view returns (uint256) {
-    return _tokenIds.current();
+  /**
+  * Get current token id.
+  */
+  function getCurrentTokenId() external view returns (uint256) {
+    return _tokenIdTracker.current();
   }
 
-  // Verify that the given leaf belongs to a given tree using its root for comparison
+  // PRIVATE METHODS
+
+  /**
+  * Verify that the given leaf belongs to a given tree 
+  * using its root for comparison.
+  */
 	function _verifyMerkleLeaf(  
     bytes32 _leafNode,  
     bytes32 _merkleRoot,  
@@ -113,19 +159,20 @@ contract Minter is ERC721, Ownable {
     return MerkleProof.verify(_proof, _merkleRoot, _leafNode); 
   }
 
-  // Create merkle leaf from supplied data
+  /**
+  * Create merkle leaf from supplied address.
+  */
   function _generateMerkleLeaf(address _account) internal pure returns (bytes32) {  
     return keccak256(abi.encodePacked(_account)); 
   }
 
-  // Mint to address
+  /**
+  * Mint to supplied address.
+  */
   function _mintTo(address to) private {
-    require(_tokenIds.current() <= AMOUNT_FOR_ALLOWLIST + AMOUNT_FOR_DEVS, "Reached max supply");
-		_safeMint(to, _tokenIds.current());
-    _tokenIds.increment();
-	}
+    require(_tokenIdTracker.current() <= AMOUNT_FOR_ALLOWLIST + AMOUNT_FOR_DEVS, "Reached max token supply");
 
-  function _baseURI() internal view virtual override returns (string memory) {
-    return _tokenBaseURI;
-  }
+		_safeMint(to, _tokenIdTracker.current());
+    _tokenIdTracker.increment();
+	}
 }
